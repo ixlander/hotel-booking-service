dd214fc6102d8aa4f18b76d0b198cac534c873f1
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2025-04-21 02:34:05 +0500

    upd

--- internal/repositories/interfaces.go
+++ internal/repositories/interfaces.go
@@ -8,26 +8,26 @@ import (
 )
 
 type UserRepository interface {
+	Create(ctx context.Context, user *data.User) error
 	FindByID(ctx context.Context, id int64) (*data.User, error)
 	FindByEmail(ctx context.Context, email string) (*data.User, error)
-	Create(ctx context.Context, user *data.User) (*data.User, error)
 }
 
 type HotelRepository interface {
-	GetAll(ctx context.Context) ([]data.Hotel, error)
+	GetAll(ctx context.Context) ([]*data.Hotel, error)
 	FindByID(ctx context.Context, id int64) (*data.Hotel, error)
 }
 
 type RoomRepository interface {
-	GetByHotelID(ctx context.Context, hotelID int64) ([]data.Room, error)
+	GetByHotelID(ctx context.Context, hotelID int64) ([]*data.Room, error)
 	FindByID(ctx context.Context, id int64) (*data.Room, error)
 	CheckAvailability(ctx context.Context, roomID int64, fromDate, toDate time.Time) (bool, error)
-	GetAvailableRooms(ctx context.Context, hotelID int64, fromDate, toDate time.Time) ([]data.Room, error)
+	GetAvailableRooms(ctx context.Context, hotelID int64, fromDate, toDate time.Time) ([]*data.Room, error)
 }
 
 type BookingRepository interface {
-	Create(ctx context.Context, booking *data.Booking) (*data.Booking, error)
+	Create(ctx context.Context, booking *data.Booking) error
 	FindByID(ctx context.Context, id int64) (*data.Booking, error)
-	FindByUserID(ctx context.Context, userID int64) ([]data.Booking, error)
+	FindByUserID(ctx context.Context, userID int64) ([]*data.Booking, error)
 	UpdateStatus(ctx context.Context, id int64, status string) error
 }
